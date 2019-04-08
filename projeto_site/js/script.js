c7caf322215e42eb6c9d9cd4a719eab2b7a788c6
jsPedro2468 <joao.pedro2468@hotmail.com>
2019-04-08 13:26:34 -0300

    Site atualizado com cadastro e login. @jsPedro2468 e @gustavocaires

---
+++ projeto_site/js/script.js
@@ -0,0 +1,24 @@
+function proximo(pag){
+    if(pag==1){
+        conteudo.style.display = 'none';
+        conteudo2.style.display = 'block';
+        conteudo3.style.display = 'none';
+        aaa.innerHTML = 'Voltar';
+    }else if(pag==2){
+        conteudo2.style.display = 'none';
+        conteudo3.style.display = 'block';
+        conteudo.style.display = 'none';
+        titulo.innerHTML = 'Informações do Galpão';
+    }else if(pag==0){
+        conteudo3.style.display = 'none';
+        conteudo.style.display = 'block';
+        conteudo2.style.display = 'none';
+        titulo.innerHTML = 'Cadastro';
+        aaa.innerHTML = 'Login';
+    }else{
+        window.location = 'login.html';
+    }
+    if(pag=="cadastro"){
+        window.location = 'cadastro.html';
+    }
+}
