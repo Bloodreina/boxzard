e136d8067961884bb8d51fd404d87d6970c6d045
jsPedro2468 <joao.pedro2468@hotmail.com>
2019-04-24 23:35:58 -0300

    required no Formulario

--- projeto_site/js/index.js
+++ projeto_site/js/index.js
@@ -1,31 +1,23 @@
 // função do simulador financeiro
 
 function calcula(){
-    var salario= (Number(sal.value) * 1.8);
-    var mostra=Number(func.value)* salario;
-    var custo=(Number(metro.value)/10)* 200;
-    var custoFormatado = custo.toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
-    var subtracao= custo + mostra;
-    var subtracaoFormatada = subtracao.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'});
+    if(func.value!= '' && sal.value !='' && metro.value!= ''){
 
-    var economia= subtracao * 12;
-    var economiaFormatada = economia.toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
-
-    if(salario <=0){
-        mensagem.innerHTML='';
-        if (func.value <=0){
-            mensagem.innerHTML='';
-        }
-        if(metro.value <=0){
-            mensagem.innerHTML='';
-
-        }
-    }
-    else{
+        var salario= (Number(sal.value) * 1.8);
+        var mostra=Number(func.value)* salario;
+        var custo=(Number(metro.value)/10)* 200;
+        var custoFormatado = custo.toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
+        var subtracao= custo + mostra;
+        var subtracaoFormatada = subtracao.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'});
+        
+        var economia= subtracao * 12;
+        var economiaFormatada = economia.toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
+        
         mensagem.innerHTML= `Adquirindo nosso sistema você economizará : ${subtracaoFormatada} por mês e ${economiaFormatada} por ano.<br> Pagando apenas o valor de ${custoFormatado} por mês .`;
         aaa.classList.add("animation");
         mensagem.style.display = "block";
         mensagem.style.opacity = 1;
+        
     }
 
 }
