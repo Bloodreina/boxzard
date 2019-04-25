0fff38f9feababbf64787ca6c4a6afaa1c3abfd1
jsPedro2468 <joao.pedro2468@hotmail.com>
2019-04-25 17:08:57 -0300

    Ajustes login

--- projeto_site/login/js/script.js
+++ projeto_site/login/js/script.js
@@ -5,7 +5,7 @@ function next(direction){
         if(direction=='front'){
 
             if(nome.value == '' || sobrenome.value == '' || email.value == '' || senha.value == '' || confirmar_senha.value == ''){
-                swal("Preencha todos os campos!", "", "warning"); //Configurar certinho o swal
+                swal("Atenção", "Preencha todos os campos!", "warning"); //Configurar certinho o swal
             }else{
                 conteudo.style.display = 'none';
                 conteudo2.style.display = 'block';
@@ -24,7 +24,7 @@ function next(direction){
         if(direction=='front'){
 
             if(cpf.value == '' || local.value == '' || numero_casa.value == '' || complemento.value == ''){
-                swal("Preencha todos os campos!", "", "warning");
+                swal("Atenção","Preencha todos os campos!", "warning");
             }else{
                 conteudo2.style.display = 'none';
                 conteudo3.style.display = 'block';
@@ -48,9 +48,9 @@ function next(direction){
 
         if(direction=='front'){
             if(tamanho.value == '' || qtd_ar.value == '' || potencia.value == ''){
-                swal("Preencha todos os campos!", "", "warning");
+                swal("Atenção", "Preencha todos os campos!", "warning");
             }else{
-                swal('Cadastro efetuado com Sucesso').then((value) =>{
+                swal("Concluído","Cadastro efetuado com sucesso!","success").then((value) =>{
     
                     window.location = 'login.html';
                 });
@@ -85,7 +85,7 @@ function logar(){
     if(user == login.value && password == senha.value){
         window.location = 'System/home.html';
     }else if(login.value == "" || senha.value == ""){
-        swal("Preencha os campos para fazer login", "", "warning");
+        swal("Atenção","Preencha todos os campos!", "warning");
     }else{
         swal("Usuário ou senha incorretos", "Preencha novamente", "warning");
     }
