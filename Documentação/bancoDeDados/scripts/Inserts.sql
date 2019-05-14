5b0df1bf64c9c20be7374ed7a5a6a737c5703125
Bloodreina <alexbuarque20@gmail.com>
2019-05-14 20:41:56 -0300

    Atualização Scripts do banco, modelo conceitual, modelo logico e restrição de login por email

--- Documentação/bancoDeDados/scripts/Inserts.sql
+++ Documentação/bancoDeDados/scripts/Inserts.sql
@@ -8,14 +8,14 @@
 	('04144110', 'SP', 'São Paulo', 'Rua Alcatrazes', 21, 'Bloco C'),
 	('04294070', 'SP', 'São Paulo', 'Rua Barão da Casa Branca', 12, 'Bloco A'),
 	('05662080', 'SP', 'Sao Paulo', 'Rua Breno Pinheiro', 53, 'Bloco D'),
-	('06470180', 'SP', 'Barueri', 'Alameda Argentina', '230', 'Casa');
+	('06470180', 'SP', 'Barueri', 'Alameda Argentina', 230, 'Casa');
 
 insert into tbUsuario values
-	('99308116052', 'Paulo Junior Oliveira', 'Paulo_Junior@gmail.com', '10Paulojunior!', '15/06/2015', 6),
-	('25972444000110', 'Maria da Silva', 'Maria_Silva@gmail.com', '01Mariasilva#', '01/03/2018', 7),
-	('49835300000180', 'Adolfo Silvestre Santos', 'Adolfo_Silvestre@gmail.com', '99Adolfosilvestre%', '20/08/2018', 8),
-	('98167975002', 'Guilherme Fonseca', 'Guilherme_Fonseca@outlook.com', '21Guilhermefonseca$', '30/11/2014', 9),
-	('80647243000155', Alessandra de Moraes', 'Alessandra_moraes@yahoo.com.br', '04Alessandramoraes*', '14/03/2018', 10);
+	('99308116052', 'PF', 'Paulo Junior Oliveira', 'Armazenamento Alpha', 'Paulo_Junior@gmail.com', '10Paulojunior!', '2015-06-15 13:18:58', 6),
+	('25972444000110', 'PJ', 'Maria da Silva', 'Armazenamento Alpha', 'Maria_Silva@gmail.com', '01Mariasilva#', '2018-03-01 13:18:58', 7),
+	('49835300000180', 'PJ', 'Adolfo Silvestre Santos', 'Armazenamento Alpha', 'Adolfo_Silvestre@gmail.com', '99Adolfosilvestre%', '2018-08-20 13:18:58', 8),
+	('98167975002', 'PF', 'Guilherme Fonseca', 'Armazenamento Alpha', 'Guilherme_Fonseca@outlook.com', '21Guilhermefonseca$', '2014-11-30 13:18:58', 9),
+	('80647243000155', 'PJ', 'Alessandra de Moraes', 'Armazenamento Alpha', 'Alessandra_moraes@yahoo.com.br', '04Alessandramoraes*', '2018-03-14 13:18:58', 10);
 
 insert into tbTelefone values
 	(9163734489, 1),
@@ -25,29 +25,29 @@ insert into tbTelefone values
 	(948261337, 5);
 
 insert into tbGalpao values
-	(300, 30, 1),
-	(150, 15, 2),
-	(200, 20, 3),
-	(400, 40, 4),
-	(100, 10, 5);
+	(300, 30, 1, 8),
+	(150, 15, 2, 6),
+	(200, 20, 3, 4),
+	(400, 40, 4, 5),
+	(100, 10, 5, 7);
 
 insert into tbSensor values
 	('DHT11', 1),
-	(TIl35', 1);
+	('TIl35', 1);
 
 insert into tbDados values
-	(2018/02/12, 20, 70, 1);
+	('2018/02/12 19:22:31', 20, 70, 1);
 
 insert into tbProduto values
-	('Alimentício', 'Banana', 28, 26, 80, 70, 1),
-	('Alimentício', 'Maça', 25, 18, 80, 70, 2),
-	('Alimentício', 'Picanha', 8, -1, 50, 20, 3),
-	('Farmacêutico', 'Insulina Regular', 30, 15, 55, 50, 4),
-	('Farmacêutico', 'Vacinas (fria ou refrigerada), 8, 2, 70, 40, 5);
+	('Alimentício', 'Banana', 26, 28, 70, 80, 1),
+	('Alimentício', 'Maça', 18, 25, 70, 80, 2),
+	('Alimentício', 'Picanha', -1, 8, 20, 50, 3),
+	('Farmacêutico', 'Insulina Regular', 15, 30, 50, 55, 4),
+	('Farmacêutico', 'Vacinas (fria ou refrigerada)', 2, 8, 40, 70, 5);
 
 insert into tbAmbiente values
-	(28, 26, 80, 70, 1),
-	(25, 18, 80, 70, 2),
-	(8, -1, 50, 20, 3),
-    	(30, 15, 55, 50, 4),
-	(7, 3, 70, 40, 5);
+	(26, 28, 70, 80, 1),
+	(18, 25, 70, 80, 2),
+	(-1, 8, 20, 50, 3),
+    (15, 30, 50, 55, 4),
+	(2, 8, 40, 70, 5);
